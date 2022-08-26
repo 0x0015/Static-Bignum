db1aa82543a7fe3654394a4289bf65fd2f99ea19
0x0015 <64482595+0x0015@users.noreply.github.com>
2022-08-25 21:54:27 -0400

    now?

--- bf_.c
+++ bf_.c
@@ -19,6 +19,8 @@ void PPCAT(bf_diagnostic_print, BN_PREFIX)(BN_VAR_PREFIX struct PPCAT(bf, BN_PRE
 
 #if BN_ARRAY_SIZE_MOD != 1
 void PPCAT(bigfloat_change_exponent, BN_PREFIX)(BN_VAR_PREFIX struct PPCAT(bf, BN_PREFIX)* n, int wantedDigits);
+#else
+void bigfloat_change_exponent_2(struct bf_2* n, int wantedDigits);
 #endif
 int PPCAT(bf_get_maxDigits, BN_PREFIX)();
 void PPCAT(bf_shiftEXP, BN_PREFIX)(BN_VAR_PREFIX struct PPCAT(bf, BN_PREFIX)* n, int shift);
