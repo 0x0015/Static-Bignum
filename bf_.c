40ff7f48de75d0a56211985b4a928ea544b57591
0x0015 <64482595+0x0015@users.noreply.github.com>
2022-08-25 21:56:18 -0400

    Update bf_.c

--- bf_.c
+++ bf_.c
@@ -17,7 +17,7 @@ void PPCAT(bf_diagnostic_print, BN_PREFIX)(BN_VAR_PREFIX struct PPCAT(bf, BN_PRE
 #define BF_IF_DIAGNOSTIC_INLINE(x) 
 #endif
 
-#if BN_ARRAY_SIZE_MOD != 1
+#if BN_ARRAY_SIZE_MOD == 1
 void PPCAT(bigfloat_change_exponent, BN_PREFIX)(BN_VAR_PREFIX struct PPCAT(bf, BN_PREFIX)* n, int wantedDigits);
 #else
 void bigfloat_change_exponent_2(struct bf_2* n, int wantedDigits);
