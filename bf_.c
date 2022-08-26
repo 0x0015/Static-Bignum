b83b025a608b01328f5941f7bfb34b8d23212f56
0x0015 <64482595+0x0015@users.noreply.github.com>
2022-08-25 21:49:51 -0400

    fixed typo

--- bf_.c
+++ bf_.c
@@ -181,7 +181,7 @@ void PPCAT(bigfloat_sub, BN_PREFIX)(BN_VAR_PREFIX struct PPCAT(bf, BN_PREFIX)* a
 //#warning using double mul/div
 //defined in the second pass with double precision
 //should always be defined as long as BN_NO_DOUBLE_P is not defined
-void bigfloat_change_exponent_2(struct bf_2*n , int wantedDigits);
+void bigfloat_change_exponent_2(struct bf_2* n , int wantedDigits);
 int bf_get_maxDigits_2();
 
 void bigfloat_mul(BN_VAR_PREFIX struct bf* a, BN_VAR_PREFIX struct bf* b, BN_VAR_PREFIX struct bf* c){
